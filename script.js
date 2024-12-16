cb8479c1c97efdfde6e6c49059642c58cf268826
videolectures101 <mathvideolectures101@gmail.com>
2024-12-16 23:25:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-function redirectToVideo() {
-    const dropdown = document.getElementById("topicDropdown");
-    const selectedValue = dropdown.value;
+// Define playlist URLs
+var playlistUrls = {
+    'Algebra': 'https://videolectures101.github.io/videolectures/',
+    'Geometry': 'https://videolectures101.github.io/geometryvideo/',
+    'Arithmetic': 'https://videolectures101.github.io/arithmetic/'
+};
 
-    if (selectedValue) {
-        window.open(selectedValue, "_blank");
-    } else {
-        alert("Please select a topic!");
+// Get DOM elements
+var topicSelector = document.getElementById('topicSelector');
+var goButton = document.getElementById('goButton');
+
+// Enable/disable button based on selection
+topicSelector.addEventListener('change', function () {
+    goButton.disabled = !this.value;
+});
+
+// Navigate to selected playlist
+goButton.addEventListener('click', function () {
+    var selectedTopic = topicSelector.value;
+    if (selectedTopic && playlistUrls[selectedTopic]) {
+        window.open(playlistUrls[selectedTopic], '_blank');
     }
-}
+});
